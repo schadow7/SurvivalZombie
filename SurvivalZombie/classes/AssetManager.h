403d2fe48b24338064eb3f5c837a7599e8101070
gitnaz <iglingorismuser@gmail.com>
2018-08-21 14:26:45 +0200

    AssetManager  commented

--- SurvivalZombie/classes/AssetManager.h
+++ SurvivalZombie/classes/AssetManager.h
@@ -2,14 +2,22 @@
 #include <SFML/Graphics.hpp>
 #include <unordered_map>
 #include <string>
-
+/*!
+ * Klasa do wspó³dzielenia assetów (w tej chwili:tekstur)
+ * 
+ */
 class AssetManager
 {
 private:
 	static std::unordered_map<std::string, std::shared_ptr<sf::Texture>> textureCatche;
 
 public:
-	
+	/*!
+	 * Sprawdza czy tekstura jest ju¿ w pamiêci.Jeœli nie, wczytuje do mapy.
+	 * 
+	 * \param name- scie¿ka do pliku tekstury, u¿ywana jako klucz do mapy
+	 * \return shared_ptr do tekstury
+	 */
 	static std::shared_ptr<sf::Texture> GetTexture(const std::string& name)
 	{
 		const auto i = textureCatche.find(name);
@@ -25,6 +33,10 @@ public:
 			return newTex;
 		}
 	}
+	/*!
+	 * Usuwa z pamiêci nieu¿ywane tekstury.
+	 * 
+	 */
 	static void Clear()
 	{
 		for (auto i = textureCatche.begin(); i != textureCatche.end(); )
