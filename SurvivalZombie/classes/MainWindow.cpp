6f713c068cbef9ec10fc047c14674b9afb3fa102
Marcin Musioł <Volfensten@gmail.com>
2018-09-22 22:37:54 +0200

    reworked menu

--- SurvivalZombie/classes/MainWindow.cpp
+++ SurvivalZombie/classes/MainWindow.cpp
@@ -9,11 +9,15 @@ MainWindow::MainWindow()
 	window = new sf::RenderWindow( sf::VideoMode( 1280, 720, 32 ), "SurvivalZombie", sf::Style::Default, settings );
 	window->setFramerateLimit( 60 );
 	cursor = new sf::Cursor;
-	gameState = 0;
+	gameState = GameState::MENU;
 
 	//Przygotowanie menu
 	selectedButton = 0;
+	mouseX = 0;
+	mouseY = 0;
+	shadow = 0;
 	clicked = false;
+	menuType = MenuType::DEFAULT;
 	loadTextures();
 	//Stworzenie gry
 	newGame();
@@ -43,19 +47,17 @@ void MainWindow::run(  )
 				loadGame( SaveSlot::SLOT_2 );
 			}
 		}
-		if ( gameState == 0 )
+		if ( gameState == GameState::MENU)
 		{
 			cursor->loadFromSystem( sf::Cursor::Arrow );
 			window->setMouseCursor( *cursor );
 			gameState = runMenu( event );
-
 		}
-		else if ( gameState == 1 )
+		else if ( gameState == GameState::RUNNING)
 		{
 			cursor->loadFromSystem( sf::Cursor::Cross );
 			window->setMouseCursor( *cursor );
 			game->runGame( window );
-			
 		}
 
 		//Wyœwietlenie obrazu
@@ -141,106 +143,288 @@ void MainWindow::loadTextures()
 	posY = 200;
 	spacing = 100;
 	width = 304;
-	height = 79;
+	height1 = 84;
+	shadow = 3;
 
 
 	//////MENU
 	AssetManager::AddTexture( "backgroundMenu", ".\\graphics\\menu\\backgroundMenu.png" );
 	backgroundMenu.setTexture( *AssetManager::GetTexture( "backgroundMenu" ) );
 
-	AssetManager::AddTexture( "button[0]", ".\\graphics\\menu\\buttonPlayOff.png" );
+	AssetManager::AddTexture( "button[0]", ".\\graphics\\menu\\buttonContinueOff.png" );
 	button[0].setTexture( *AssetManager::GetTexture( "button[0]" ) );
 	button[0].setPosition( sf::Vector2f( posX, posY ) );
-
-	AssetManager::AddTexture( "button[1]", ".\\graphics\\menu\\buttonPlayOn.png" );
+	AssetManager::AddTexture( "button[1]", ".\\graphics\\menu\\buttonContinueOn.png" );
 	button[1].setTexture( *AssetManager::GetTexture( "button[1]" ) );
 	button[1].setPosition( sf::Vector2f( posX, posY ) );
 
-	AssetManager::AddTexture("button[2]", ".\\graphics\\menu\\buttonOptionsOff.png");
+	AssetManager::AddTexture("button[2]", ".\\graphics\\menu\\buttonNewGameOff.png");
 	button[2].setTexture( *AssetManager::GetTexture( "button[2]") );
 	button[2].setPosition( sf::Vector2f( posX, posY + spacing ) );
-
-	AssetManager::AddTexture("button[3]", ".\\graphics\\menu\\buttonOptionsOn.png");
+	AssetManager::AddTexture("button[3]", ".\\graphics\\menu\\buttonNewGameOn.png");
 	button[3].setTexture( *AssetManager::GetTexture( "button[3]" ) );
 	button[3].setPosition( sf::Vector2f( posX, posY + spacing ) );
-	
-	AssetManager::AddTexture("button[4]", ".\\graphics\\menu\\buttonExitOff.png");
-	button[4].setTexture( *AssetManager::GetTexture( "button[4]" ) );
-	button[4].setPosition( sf::Vector2f( posX, posY + spacing * 2 ) );
-	
-	AssetManager::AddTexture("button[5]", ".\\graphics\\menu\\buttonExitOn.png");
-	button[5].setTexture( *AssetManager::GetTexture( "button[5]" ) );
-	button[5].setPosition( sf::Vector2f( posX, posY + spacing * 2 ) );
+
+	AssetManager::AddTexture("button[4]", ".\\graphics\\menu\\buttonLoadGameOff.png");
+	button[4].setTexture(*AssetManager::GetTexture("button[4]"));
+	button[4].setPosition(sf::Vector2f(posX, posY + spacing * 2));
+	AssetManager::AddTexture("button[5]", ".\\graphics\\menu\\buttonLoadGameOn.png");
+	button[5].setTexture(*AssetManager::GetTexture("button[5]"));
+	button[5].setPosition(sf::Vector2f(posX, posY + spacing * 2));
+
+	AssetManager::AddTexture("button[6]", ".\\graphics\\menu\\buttonExitOff.png");
+	button[6].setTexture(*AssetManager::GetTexture("button[6]"));
+	button[6].setPosition(sf::Vector2f(posX, posY + spacing * 3));
+	AssetManager::AddTexture("button[7]", ".\\graphics\\menu\\buttonExitOn.png");
+	button[7].setTexture(*AssetManager::GetTexture("button[7]"));
+	button[7].setPosition(sf::Vector2f(posX, posY + spacing * 3));
+
+	height2 = 134;
+	dposY = -100;
+	dspacing = 1.5;
+	AssetManager::AddTexture("button[8]", ".\\graphics\\menu\\buttonSlot1Off.png");
+	button[8].setTexture(*AssetManager::GetTexture("button[8]"));
+	button[8].setPosition(sf::Vector2f(posX, posY + dposY));
+	AssetManager::AddTexture("button[9]", ".\\graphics\\menu\\buttonSlot1On.png");
+	button[9].setTexture(*AssetManager::GetTexture("button[9]"));
+	button[9].setPosition(sf::Vector2f(posX, posY + dposY));
+
+	AssetManager::AddTexture("button[10]", ".\\graphics\\menu\\buttonSlot2Off.png");
+	button[10].setTexture(*AssetManager::GetTexture("button[10]"));
+	button[10].setPosition(sf::Vector2f(posX, posY + dposY + spacing * dspacing));
+	AssetManager::AddTexture("button[11]", ".\\graphics\\menu\\buttonSlot2On.png");
+	button[11].setTexture(*AssetManager::GetTexture("button[11]"));
+	button[11].setPosition(sf::Vector2f(posX, posY + dposY + spacing * dspacing));
+
+	AssetManager::AddTexture("button[12]", ".\\graphics\\menu\\buttonSlot3Off.png");
+	button[12].setTexture(*AssetManager::GetTexture("button[12]"));
+	button[12].setPosition(sf::Vector2f(posX, posY + dposY + spacing * dspacing * 2));
+	AssetManager::AddTexture("button[13]", ".\\graphics\\menu\\buttonSlot3On.png");
+	button[13].setTexture(*AssetManager::GetTexture("button[13]"));
+	button[13].setPosition(sf::Vector2f(posX, posY + dposY + spacing * dspacing * 2));
+
+	AssetManager::AddTexture("button[14]", ".\\graphics\\menu\\buttonReturnOff.png");
+	button[14].setTexture(*AssetManager::GetTexture("button[14]"));
+	button[14].setPosition(sf::Vector2f(posX, posY + dposY + spacing * dspacing * 3));
+	AssetManager::AddTexture("button[15]", ".\\graphics\\menu\\buttonReturnOn.png");
+	button[15].setTexture(*AssetManager::GetTexture("button[15]"));
+	button[15].setPosition(sf::Vector2f(posX, posY + dposY + spacing * dspacing * 3));
 }
 
 
-int MainWindow::runMenu( sf::Event event )
+GameState MainWindow::runMenu( sf::Event event )
 {
 	if ( !clicked && sf::Mouse::isButtonPressed( sf::Mouse::Left ) )
 	{
-		int mouseX = sf::Mouse::getPosition( *window ).x;
-		int mouseY = sf::Mouse::getPosition( *window ).y;
+		mouseX = sf::Mouse::getPosition( *window ).x;
+		mouseY = sf::Mouse::getPosition( *window ).y;
 
-		if ( ( mouseX >= posX && mouseX <= posX + width ) &&
-			( mouseY >= posY && mouseY <= posY + height ) )
-		{
-			selectedButton = 1;
-			clicked = true;
-		}
-		else if ( ( mouseX >= posX && mouseX <= posX + width ) &&
-			( mouseY >= posY + spacing && mouseY <= posY + spacing + height ) )
-		{
-			selectedButton = 3;
-			clicked = true;
-		}
-		else if ( ( mouseX >= posX && mouseX <= posX + width ) &&
-			( mouseY >= posY + spacing * 2 && mouseY <= posY + spacing * 2 + height ) )
-		{
-			selectedButton = 5;
-			clicked = true;
-		}
+		if(menuType == MenuType::DEFAULT) runDefaultMenu();
+		else if (menuType == MenuType::NEWGAME) runNewGameMenu();
+		else if (menuType == MenuType::LOADGAME) runLoadGameMenu();
 	}
 
+	gameState = GameState::MENU;
 	if ( clicked && event.type == sf::Event::MouseButtonReleased )
 	{
 		if ( event.mouseButton.button == sf::Mouse::Left )
 		{
-			int mouseX = sf::Mouse::getPosition( *window ).x;
-			int mouseY = sf::Mouse::getPosition( *window ).y;
+			mouseX = sf::Mouse::getPosition( *window ).x;
+			mouseY = sf::Mouse::getPosition( *window ).y;
 			clicked = false;
 
-			if ( ( mouseX >= posX && mouseX <= posX + width ) &&
-				( mouseY >= posY && mouseY <= posY + height ) )
-			{
-				return 1;
-			}
-			else if ( ( mouseX >= posX && mouseX <= posX + width ) &&
-				( mouseY >= posY + spacing && mouseY <= posY + spacing + height ) )
-			{
-				selectedButton = 3;
-			}
-			else if ( ( mouseX >= posX && mouseX <= posX + width ) &&
-				( mouseY >= posY + spacing * 2 && mouseY <= posY + spacing * 2 + height ) )
-			{
-				window->close();
-			}
+			if (menuType == MenuType::DEFAULT) gameState = runDefaultMenuClicked();
+			else if (menuType == MenuType::NEWGAME) gameState = runNewGameMenuClicked();
+			else if (menuType == MenuType::LOADGAME) gameState = runLoadGameMenuClicked();
 		}
 	}
 
 	this->draw();
 
-	return 0;
+	return gameState;
 }
 
+void MainWindow::runDefaultMenu()
+{
+	if ((mouseX >= shadow + posX && mouseX <= shadow + posX + width) &&
+		(mouseY >= shadow + posY && mouseY <= shadow + posY + height1))
+	{
+		selectedButton = 1;
+		clicked = true;
+	}
+	else if ((mouseX >= shadow + posX && mouseX <= shadow + posX + width) &&
+		(mouseY >= shadow + posY + spacing && mouseY <= shadow + posY + spacing + height1))
+	{
+		selectedButton = 3;
+		clicked = true;
+	}
+	else if ((mouseX >= shadow + posX && mouseX <= shadow + posX + width) &&
+		(mouseY >= shadow + posY + spacing * 2 && mouseY <= shadow + posY + spacing * 2 + height1))
+	{
+		selectedButton = 5;
+		clicked = true;
+	}
+	else if ((mouseX >= shadow + posX && mouseX <= shadow + posX + width) &&
+		(mouseY >= shadow + posY + spacing * 3 && mouseY <= shadow + posY + spacing * 3 + height1))
+	{
+		selectedButton = 7;
+		clicked = true;
+	}
+}
 
+void MainWindow::runNewGameMenu()
+{
+	if ((mouseX >= shadow + posX && mouseX <= shadow + posX + width) &&
+		(mouseY >= shadow + posY + dposY && mouseY <= shadow + posY + dposY + height2))
+	{
+		selectedButton = 9;
+		clicked = true;
+	}
+	else if ((mouseX >= shadow + posX && mouseX <= shadow + posX + width) &&
+		(mouseY >= shadow + posY + dposY + spacing * dspacing && mouseY <= shadow + posY + dposY + spacing * dspacing + height2))
+	{
+		selectedButton = 11;
+		clicked = true;
+	}
+	else if ((mouseX >= shadow + posX && mouseX <= shadow + posX + width) &&
+		(mouseY >= shadow + posY + dposY + spacing * dspacing * 2 && mouseY <= shadow + posY + dposY + spacing * dspacing * 2 + height2))
+	{
+		selectedButton = 13;
+		clicked = true;
+	}
+	else if ((mouseX >= shadow + posX && mouseX <= shadow + posX + width) &&
+		(mouseY >= shadow + posY + dposY + spacing * dspacing * 3 && mouseY <= shadow + posY + dposY + spacing * dspacing * 3 + height1))
+	{
+		selectedButton = 15;
+		clicked = true;
+	}
+}
 
+void MainWindow::runLoadGameMenu()
+{
+	if ((mouseX >= shadow + posX && mouseX <= shadow + posX + width) &&
+		(mouseY >= shadow + posY + dposY && mouseY <= shadow + posY + dposY + height2))
+	{
+		selectedButton = 9;
+		clicked = true;
+	}
+	else if ((mouseX >= shadow + posX && mouseX <= shadow + posX + width) &&
+		(mouseY >= shadow + posY + dposY + spacing * dspacing && mouseY <= shadow + posY + dposY + spacing * dspacing + height2))
+	{
+		selectedButton = 11;
+		clicked = true;
+	}
+	else if ((mouseX >= shadow + posX && mouseX <= shadow + posX + width) &&
+		(mouseY >= shadow + posY + dposY + spacing * dspacing * 2 && mouseY <= shadow + posY + dposY + spacing * dspacing * 2 + height2))
+	{
+		selectedButton = 13;
+		clicked = true;
+	}
+	else if ((mouseX >= shadow + posX && mouseX <= shadow + posX + width) &&
+		(mouseY >= shadow + posY + dposY + spacing * dspacing * 3 && mouseY <= shadow + posY + dposY + spacing * dspacing * 3 + height1))
+	{
+		selectedButton = 15;
+		clicked = true;
+	}
+}
+
+GameState MainWindow::runDefaultMenuClicked()
+{
+	if ((mouseX >= shadow + posX && mouseX <= shadow + posX + width) &&
+		(mouseY >= shadow + posY && mouseY <= shadow + posY + height1))
+	{
+		return GameState::RUNNING;
+	}
+	else if ((mouseX >= shadow + posX && mouseX <= shadow + posX + width) &&
+		(mouseY >= shadow + posY + spacing && mouseY <= shadow + posY + spacing + height1))
+	{
+		menuType = MenuType::NEWGAME;
+	}
+	else if ((mouseX >= shadow + posX && mouseX <= shadow + posX + width) &&
+		(mouseY >= shadow + posY + spacing * 2 && mouseY <= shadow + posY + spacing * 2 + height1))
+	{
+		menuType = MenuType::LOADGAME;
+	}
+	else if ((mouseX >= shadow + posX && mouseX <= shadow + posX + width) &&
+		(mouseY >= shadow + posY + spacing * 3 && mouseY <= shadow + posY + spacing * 3 + height1))
+	{
+		window->close();
+	}
+
+	return GameState::MENU;
+}
+
+GameState MainWindow::runNewGameMenuClicked()
+{
+	if ((mouseX >= shadow + posX && mouseX <= shadow + posX + width) &&
+		(mouseY >= shadow + posY + dposY && mouseY <= shadow + posY + dposY + height2))
+	{
+		return GameState::RUNNING;
+	}
+	else if ((mouseX >= shadow + posX && mouseX <= shadow + posX + width) &&
+		(mouseY >= shadow + posY + dposY + spacing * dspacing && mouseY <= shadow + posY + dposY + spacing * dspacing + height2))
+	{
+		return GameState::RUNNING;
+	}
+	else if ((mouseX >= shadow + posX && mouseX <= shadow + posX + width) &&
+		(mouseY >= shadow + posY + dposY + spacing * dspacing * 2 && mouseY <= shadow + posY + dposY + spacing * dspacing * 2 + height2))
+	{
+		return GameState::RUNNING;
+	}
+	else if ((mouseX >= shadow + posX && mouseX <= shadow + posX + width) &&
+		(mouseY >= shadow + posY + dposY + spacing * dspacing * 3 && mouseY <= shadow + posY + dposY + spacing * dspacing * 3 + height1))
+	{
+		menuType = MenuType::DEFAULT;
+	}
+
+	return GameState::MENU;
+}
+
+GameState MainWindow::runLoadGameMenuClicked()
+{
+	if ((mouseX >= shadow + posX && mouseX <= shadow + posX + width) &&
+		(mouseY >= shadow + posY + dposY && mouseY <= shadow + posY + dposY + height2))
+	{
+		return GameState::RUNNING;
+	}
+	else if ((mouseX >= shadow + posX && mouseX <= shadow + posX + width) &&
+		(mouseY >= shadow + posY + dposY + spacing * dspacing && mouseY <= shadow + posY + dposY + spacing * dspacing + height2))
+	{
+		return GameState::RUNNING;
+	}
+	else if ((mouseX >= shadow + posX && mouseX <= shadow + posX + width) &&
+		(mouseY >= shadow + posY + dposY + spacing * dspacing * 2 && mouseY <= shadow + posY + dposY + spacing * dspacing * 2 + height2))
+	{
+		return GameState::RUNNING;
+	}
+	else if ((mouseX >= shadow + posX && mouseX <= shadow + posX + width) &&
+		(mouseY >= shadow + posY + dposY + spacing * dspacing * 3 && mouseY <= shadow + posY + dposY + spacing * dspacing * 3 + height1))
+	{
+		menuType = MenuType::DEFAULT;
+	}
+
+	return GameState::MENU;
+}
 
 void MainWindow::draw()
 {
 	window->draw( backgroundMenu );
-	for ( int i = 0; i < MAX_NUMBER_OF_ITEMS; i++ )
-	{
-		window->draw( button[i * 2] );
-	}
+
+	if (menuType == MenuType::DEFAULT)
+		for (int i = 0; i < 4; i++)
+		{
+			window->draw(button[i * 2]);
+		}
+	else if (menuType == MenuType::NEWGAME)
+		for (int i = 4; i < 8; i++)
+		{
+			window->draw(button[i * 2]);
+		}
+	else if (menuType == MenuType::LOADGAME)
+		for (int i = 4; i < 8; i++)
+		{
+			window->draw(button[i * 2]);
+		}
+
 	if ( clicked ) window->draw( button[selectedButton] );
 }
--- SurvivalZombie/classes/MainWindow.h
+++ SurvivalZombie/classes/MainWindow.h
@@ -5,9 +5,11 @@
 #include <string>
 #include <fstream>
 
-#define MAX_NUMBER_OF_ITEMS 3
+#define MAX_NUMBER_OF_ITEMS 8
 
 enum class SaveSlot { SLOT_1 = 1, SLOT_2 = 2, SLOT_3 = 3 };
+enum class GameState { MENU, RUNNING };
+enum class MenuType { DEFAULT, NEWGAME, LOADGAME };
 
 class MainWindow
 {
@@ -15,21 +17,31 @@ public:
 	MainWindow();
 	~MainWindow();
 
+
 	void									run();
 	void									newGame();
 	void									loadGame( SaveSlot slot );
 	void									saveGame( SaveSlot slot );
 	void									draw();
-	int										runMenu(sf::Event event);
+	GameState								runMenu(sf::Event event);
 
 private:
 	void									loadTextures();
+	void									runDefaultMenu();
+	void									runNewGameMenu();
+	void									runLoadGameMenu();
+	GameState								runDefaultMenuClicked();
+	GameState								runNewGameMenuClicked();
+	GameState								runLoadGameMenuClicked();
 
 	sf::RenderWindow *						window;
 	Game *									game;
 	sf::Sprite								backgroundMenu, button[MAX_NUMBER_OF_ITEMS * 2];
-	int										posX, posY, spacing, width, height, selectedButton;
+	int										shadow, posX, posY, spacing, dposY, width, height1, height2, mouseX, mouseY, selectedButton;
+	double									dspacing;
 	bool									clicked;
-	int										gameState;
+	int										action;
 	sf::Cursor *							cursor;
+	MenuType								menuType;
+	GameState								gameState;
 };
