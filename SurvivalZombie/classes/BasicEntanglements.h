343f0b3716a07ffd3a0dfdf63a08abd0ae2309ce
gitnaz <iglingorismuser@gmail.com>
2018-09-25 18:00:40 +0200

    BasicEntagelments and Obstacle commentary

--- SurvivalZombie/classes/BasicEntanglements.h
+++ SurvivalZombie/classes/BasicEntanglements.h
@@ -5,7 +5,14 @@ class BasicEntanglements :
 	public StaticBody
 {
 public:
-	BasicEntanglements(b2World* world, b2Vec2 position, sf::Vector2i size = sf::Vector2i(25,50), sf::Texture* ntexture = AssetManager::GetTexture("grad2"));
+	/// <summary>
+	/// Initializes a new instance of the <see cref="BasicEntanglements"/> class.
+	/// </summary>
+	/// <param name="world">The b2World.</param>
+	/// <param name="position">The position in b2World.</param>
+	/// <param name="size">The size in px.</param>
+	/// <param name="ntexture">The new texture.</param>
+	BasicEntanglements(b2World* world, b2Vec2 position, sf::Vector2i size = sf::Vector2i(25, 50), sf::Texture* ntexture = AssetManager::GetTexture("grad2"));
 	~BasicEntanglements();
 	void StartContact(Entity*);
 	void EndContact(Entity*);
@@ -14,15 +21,54 @@ public:
 	void RenderInactive(sf::RenderWindow* window);
 	void Update(sf::Time);
 	void TakeDamage(float32 damage) override;
+	/// <summary>
+	/// Attacks the specified entity. Uses TakeDamege()
+	/// </summary>
+	/// <param name="entity">The entity.</param>
 	virtual void Attack(Entity * entity);
+	/// <summary>
+	/// Ustawia iloœæ zadawanych obra¿eñ
+	/// </summary>
+	/// <param name="">Obra¿enia .</param>
 	void SetDamage(float32);
+	/// <summary>
+	/// Ustawia k¹t jaki obiekt obiekt na mapie
+	/// </summary>
+	/// <param name="">The .</param>
 	void SetAngle(float32);
+	/// <summary>
+	/// Zwraca rozmiar w pikselach
+	/// </summary>
+	/// <returns></returns>
 	b2Vec2 GetPxSize();
+	/// <summary>
+	/// Podnosi wytrzyma³oœæ o podan¹ liczba albo do maksimum
+	/// </summary>
+	/// <param name=""> .</param>
 	void Repair(int);
+	/// <summary>
+	/// Wstawia maksymaln¹ wytrzyma³oœæ barykady
+	/// </summary>
+	/// <param name=""> .</param>
 	void SetMaxHP(int);
+	/// <summary>
+	/// Ustawia obra¿enia jakie barykada zadaje atakuj¹cym j¹ obiektom
+	/// </summary>
+	/// <param name=""> .</param>
 	void SetDamage(int);
+	/// <summary>
+	/// Aktywuje obiekt jeœli jest nieaktywny.
+	/// </summary>
 	void MakeActive();
+	/// <summary>
+	/// Pobiera stan wytrzyma³oœci.
+	/// </summary>
+	/// <returns></returns>
 	int getHitpoints();
+	/// <summary>
+	/// Pobiera maksymaln ¹wytrzyma³oœæ.
+	/// </summary>
+	/// <returns></returns>
 	int getMaxHitpoints();
 
 protected:
--- SurvivalZombie/classes/Obstacle.h
+++ SurvivalZombie/classes/Obstacle.h
@@ -4,7 +4,14 @@ class Obstacle :
 	public StaticBody
 {
 public:
-	Obstacle(b2World* world, b2Vec2 position,sf::Vector2i size = sf::Vector2i(50,50), sf::Texture * ntexture = AssetManager::GetTexture("grad2"));
+	/// <summary>
+	/// Initializes a new instance of the <see cref="Obstacle"/> class.
+	/// </summary>
+	/// <param name="world">The b2World.</param>
+	/// <param name="position">The position in b2World.</param>
+	/// <param name="size">The size in px.</param>
+	/// <param name="ntexture">The new texture.</param>
+	Obstacle(b2World* world, b2Vec2 position, sf::Vector2i size = sf::Vector2i(50, 50), sf::Texture * ntexture = AssetManager::GetTexture("grad2"));
 	~Obstacle();
 	void StartContact(Entity*);
 	void EndContact(Entity*);
